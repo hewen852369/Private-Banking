ebec434523a8d8ab5d9f2f382f7987dc68354705
Wen He <hewen852369@hotmail.com>
2016-09-30 21:49:54 -0500

    Implemented User home page Update Contact and About Page Implement regular login and register

---
+++ PrivateBanking/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PrivateBanking.Startup))]
+namespace PrivateBanking
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
